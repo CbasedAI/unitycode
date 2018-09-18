6f872117ca599896da7cb8e5fa440b8199288b58
artinhuang <artinhuangcn@gmail.com>
2018-09-18 22:32:11 +0800

    update

---
+++ 001-Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	// use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Upadate is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		GetComponent<Rigidbody> ().AddForce (movement * speed * Time.deltaTime);
+//		rb.AddForce (movement * speed);
+	}
+}
